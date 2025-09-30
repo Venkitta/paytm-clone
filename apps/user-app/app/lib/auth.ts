7d7ee1d42033f88f04ff7e51df2a15b51762c692
Anas <anasvk38@gmail.com>
2025-09-30 21:22:55 +0530

    updated env variables

--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -38,7 +38,7 @@ export const authOptions = {
           },
         })
     ],
-    secret: process.env.NEXTAUTH_SECRET || "secret",
+    secret: process.env.NEXTAUTH_SECRET,
     pages: {
         signIn: '/auth/signin'
     },
