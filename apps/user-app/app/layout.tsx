13be53b50e3336c16d8d9c5f45e5f578550a5511
Anas <anasvk38@gmail.com>
2025-10-01 17:39:39 +0530

    title updated

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -7,7 +7,7 @@ import { AppbarClient } from "../components/AppbarClient";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "My PayTM Clone",
+  title: "My PayTM App",
   description: "Simple wallet app",
 };
 
